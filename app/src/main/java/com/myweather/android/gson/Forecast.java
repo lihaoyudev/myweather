32b55b96dc8959d53e0541ad7b694ac77e8ccef2
Li <987434144@qq.com>
2017-12-08 21:27:48 +0800

    遍历完省份等信息

---
+++ app/src/main/java/com/myweather/android/gson/Forecast.java
@@ -0,0 +1,31 @@
+package com.myweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Administrator on 2017/12/8.
+ */
+
+public class Forecast {
+
+    public String date;
+
+    @SerializedName("tmp")
+    public Temperature temperature;
+
+    @SerializedName("cond")
+    public More more;
+
+    private class Temperature {
+
+        public String max;
+
+        public String min;
+    }
+
+    private class More {
+
+        @SerializedName("txt_d")
+        public String info;
+    }
+}
---
+++ app/src/main/java/com/myweather/android/gson/Weather.java
@@ -0,0 +1,25 @@
+package com.myweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/12/8.
+ */
+
+public class Weather {
+
+    public String status;
+
+    public Basic basic;
+
+    public AQI aqi;
+
+    public Now now;
+
+    public Suggestion suggestion;
+
+    @SerializedName("daliy_forecast")
+    public List<Forecast> forecastList;
+}
